4d84e14f853b1de30b3d5f6c12e46a42304e9277
Laur4xl <107406784+Laur4xl@users.noreply.github.com>
2023-11-09 08:16:05 -0300

    321

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 function tocasom(idElementoAudio){
 document.querySelectorAll(idElementoAudio).play();
 }
-const listaDeTeclas = documen.querySelectorAll("tecla")
+const listaDeTeclas = documen.querySelectorAll("tecla");
+
+let contador = 0;
+while(contador<listaDeTeclas.lenght){
+    const efeito = listaDeTeclas[contador].classList[1];
+    const idAudio = "ssom_"+efeito;
+    listaDeTeclas[contador].onclick = function(){
+        tocasom("#som_tecla_aplausos");
+        tocasom(idAudio);
+    }
+    contador = contador + 1;
+    console.log(contador);
+    
+}
